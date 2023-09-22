9dc85996b01dd9bdf1884f292f7bf03babb9668e
sthavirprabhu <145367929+sthavirprabhu@users.noreply.github.com>
2023-09-22 17:11:52 +0400

    Add files via upload

---
+++ Class 9/sketch.js
@@ -0,0 +1,17 @@
+var Pen
+function setup() {
+  createCanvas(400,400);
+  Pen=createSprite(20,20,400,232)
+  Pen.shapeColor = "red"
+  
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
